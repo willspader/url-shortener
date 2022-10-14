fe6432bf2453eb6d694e7d526db0fa1869ffb8e8
William Cabral Donato Spader <spaderwilliam@hotmail.com>
2022-10-14 20:18:20 -0300

    add gin framework

--- app/start.go
+++ app/start.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"net/http"
+)
+
+type url struct {
+	Url string `json:"url"`
+}
 
 func main() {
-	fmt.Println("start-server")
+	router := gin.Default()
+
+	router.GET("/", checkUpServer)
+
+	router.POST("/url", longUrlToShortUrl)
+
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func longUrlToShortUrl(c *gin.Context) {
+	var longUrl url
+
+	if err := c.BindJSON(&longUrl); err != nil {
+		return // TODO
+	}
+
+	fmt.Println(longUrl)
+
+	c.JSON(http.StatusOK, longUrl)
+}
+
+func checkUpServer(c *gin.Context) {
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "UP",
+	})
+
 }
