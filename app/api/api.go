c182650b0663937ab76d629df666024f92a467ff
William Cabral Donato Spader <spaderwilliam@hotmail.com>
2022-10-17 20:22:41 -0300

    api error handler

--- app/api/api.go
+++ app/api/api.go
@@ -8,21 +8,23 @@ import (
 	"url-shortener/app/types"
 )
 
+// LongUrlToShortUrl take a url and return its short representation.
+// It expect a valid json request to deserialize to types.Url struct.
+// It returns http status 200 and types.Url when everything went okay.
+// It returns http status 422 and types.Message in deserialization errors.
 func LongUrlToShortUrl(c *gin.Context) {
 	var longUrl types.Url
 
 	if err := c.BindJSON(&longUrl); err != nil {
-		c.JSON(http.StatusBadRequest, err) // TODO better return message
+		c.JSON(http.StatusBadRequest, types.Message{Msg: "Deserialization Error"})
 		return
 	}
 
 	c.JSON(http.StatusOK, longUrl)
 }
 
+// CheckUpServer can be used to know if the server is running, like a health check endpoint.
+// It returns http status 200 and a dummy types.Message if the server is up.
 func CheckUpServer(c *gin.Context) {
-
-	c.JSON(http.StatusOK, gin.H{
-		"status": "UP",
-	})
-
+	c.JSON(http.StatusOK, types.Message{Msg: "UP"})
 }
---
+++ app/types/message.go
@@ -0,0 +1,5 @@
+package types
+
+type Message struct {
+	Msg string `json:"msg"`
+}
